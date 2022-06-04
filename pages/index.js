01e9a566a572b48a14624a0cdbe460fdbb2ba620
Clayton Davis <claytondus@gmail.com>
2022-06-04 12:11:07 -0400

    prettier -change name

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
-import Link from 'next/link';
-import Date from '../components/date';
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -20,9 +20,9 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Clayton Davis</p>
+        <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
@@ -32,14 +32,14 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-  <Link href={`/posts/${id}`}>
-    <a>{title}</a>
-  </Link>
-  <br />
-  <small className={utilStyles.lightText}>
-    <Date dateString={date} />
-  </small>
-</li>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
           ))}
         </ul>
       </section>
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,8 @@
 import Layout from "../../components/layout";
-import { getAllPostIds, getPostData } from '../../lib/posts';
-import Date from '../../components/date';
-import Head from 'next/head';
-import utilStyles from '../../styles/utils.module.css';
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Date from "../../components/date";
+import Head from "next/head";
+import utilStyles from "../../styles/utils.module.css";
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -24,13 +24,13 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <Layout>
-        <Head>
-            <title>{postData.title}</title>
-        </Head>
-        <article>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
         <div className={utilStyles.lightText}>
-            <Date dateString={postData.date} />
+          <Date dateString={postData.date} />
         </div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
